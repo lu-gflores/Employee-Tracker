3b298728fa3bc2a8f340e08a01477132004e9c3f
George Flores <georgeflores886@gmail.com>
2020-06-04 18:23:22 -0400

    created database and tables

--- employee.sql
+++ employee.sql
@@ -1,26 +1,69 @@
+DROP DATABASE IF EXISTS employee_manager_db;
+
 CREATE DATABASE employee_manager_db;
 USE employee_manager_db;
 
 CREATE TABLE department (
-	id INT,
+	id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INT,
+id INTEGER NOT NULL AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
-department_id int,
+department_id INTEGER,
 PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-id INT, 
+id INTEGER NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
-role_id INT,
-manager_id int,
+role_id INTEGER,
+manager_id INTEGER,
 PRIMARY KEY (id)
-)
+);
+-- Inserting data to tables
+-- Department
+INSERT INTO department (name)
+VALUES ("Sales");
 
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+-- ROLE
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineering", 60000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales", 45000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Financing", 55000, 3);
+
+-- Employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Leo", "Hope", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dan", "Smith", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sam", "Danielson", 1, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Juan", "Lopez", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mary", "Poppins", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mary", "Sue", 2, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Wick", 3, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Martha", "Stevens", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Donovan", "Love", 2, null);
--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 
+const mainPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: 'userChoice',
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all Employees by Manager",
+                "Add Employee",
+                "Add Emloyee Role",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role"
+            ]
+        }
+    ]).then(function(answer) {
+        
+    })
+}
+mainPrompt();
+
