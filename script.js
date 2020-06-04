d35504607fd6ade7291d0b86f0971c99ec380e74
George Flores <georgeflores886@gmail.com>
2020-06-04 19:35:46 -0400

    created add employee function

--- script.js
+++ script.js
@@ -32,16 +32,17 @@ const mainPrompt = () => {
                 "Remove Employee",
                 "Update Employee Role"
             ]
-        }
-    ]).then(function(answer) {
-        if(answer.userChoice === "View all employees"){
+        }]).then(function (answer) {
+        if (answer.userChoice === "View all employees") {
             selectAllEmployees();
         } else if (answer.userChoice === "View all roles") {
             selectAllRoles();
         } else if (answer.userChoice === "View all departments") {
             selectAllDepartments();
+        } else if (answer.userChoice === "Add Employee") {
+            addEmployee();
         }
-    })
+    });
 }
 //Select all queries
 const selectAllEmployees = () => {
@@ -51,17 +52,43 @@ const selectAllEmployees = () => {
     })
     mainPrompt();
 }
-const selectAllRoles = () =>{
+const selectAllRoles = () => {
     connection.query("SELECT * FROM role", (err, result) => {
         if (err) throw err;
         console.table(result);
     })
     mainPrompt();
 }
-const selectAllDepartments = () =>{
+const selectAllDepartments = () => {
     connection.query("SELECT * FROM department", (err, result) => {
         if (err) throw err;
         console.table(result);
     })
     mainPrompt();
+}
+//adding items to tables
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter Employee's first name",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message: "Enter Employee's last name",
+            name: "lastName"
+        }
+    ]).then(function (answer) {
+        let query = connection.query(
+            "INSERT INTO employee SET first_name=?, last_name=?",
+            [answer.firstName, answer.lastName],
+            function(err, result) {
+                if(err) throw err;
+                console.log(result.affectedRows + " added as employee\n");
+                mainPrompt();
+            }
+        )
+    })
+    
 }
