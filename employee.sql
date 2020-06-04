4dd96bcf4b4098bf26b4fcc9e584e277ae29b331
George Flores <georgeflores886@gmail.com>
2020-06-04 15:26:01 -0400

    adding files to repo

---
+++ employee.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE employee_manager_db;
+USE employee_manager_db;
+
+CREATE TABLE department (
+	id INT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id int,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT, 
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id int,
+PRIMARY KEY (id)
+)
+
