7a274f0a3d1ff75974d18385cc625658922b97ef
George Flores <georgeflores886@gmail.com>
2020-06-07 21:04:41 -0400

    commented out foreign keys due to constraint

--- employee.sql
+++ employee.sql
@@ -14,8 +14,8 @@ id INTEGER AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INTEGER,
-PRIMARY KEY (id),
-FOREIGN KEY (department_id) REFERENCES department(id)
+PRIMARY KEY (id)
+-- FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -24,9 +24,9 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INTEGER,
 manager_id INTEGER,
-PRIMARY KEY (id),
-FOREIGN KEY (role_id) REFERENCES role(id),
-FOREIGN KEY (manager_id) REFERENCES employee(id)
+PRIMARY KEY (id)
+-- FOREIGN KEY (role_id) REFERENCES role(id),
+-- FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 -- Inserting data to tables
 -- Department
